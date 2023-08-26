1fae44a19beb803b3dd90e4fc5995e5f127d103e
Ayush Jain <ayushjain2205@gmail.com>
2023-08-27 00:00:07 +0530

    Add dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  OptimismGoerli,
+  LineaTestnet,
+  Ethereum,
+  Polygon,
+} from "@thirdweb-dev/chains";
+import { metamaskWallet, localWallet, paperWallet } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      activeChain={Ethereum}
+      clientId="7a96064a5047b6af0b9b1125a11562ca"
+      supportedWallets={[
+        metamaskWallet(),
+        localWallet(),
+        paperWallet({
+          paperClientId: "7f3a5a69-a739-40cd-b62d-8ef3667a81b8",
+        }),
+      ]}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
